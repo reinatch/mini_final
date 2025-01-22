22940598c6de04755f35ef053774dc4fab669c8a
reinatch <reinatchnatch@gmail.com>
2025-01-22 13:39:58 +0000

    fix open quotes

--- src/main/input_error.c
+++ src/main/input_error.c
@@ -6,11 +6,42 @@
 /*   By: rerodrig <rerodrig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 15:45:56 by rerodrig          #+#    #+#             */
-/*   Updated: 2025/01/22 00:13:46 by rerodrig         ###   ########.fr       */
+/*   Updated: 2025/01/22 13:24:07 by rerodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static int	print_error(void)
+{
+	ft_putstr_fd("You forgot to close some quotes 🌝\n", STDERR_FILENO);
+	return (1);
+}
+
+static int	has_unclosed_quotes(char *str)
+{
+	char	last_opened;
+
+	last_opened = 0;
+	while (*str && !last_opened)
+	{
+		if (*str == '\'' || *str == '"')
+			last_opened = *str;
+		str++;
+	}
+	while (*str && last_opened)
+	{
+		if (*str && *str == last_opened)
+			last_opened = 0;
+		str++;
+	}
+	if (*str)
+		return (has_unclosed_quotes(str));
+	else if (!last_opened)
+		return (0);
+	else
+		return (print_error());
+}
 
 int	is_valid_input(char *input, int *exit_status, t_env_var_node *env_var_list)
 {
@@ -18,6 +49,8 @@ int	is_valid_input(char *input, int *exit_status, t_env_var_node *env_var_list)
 
 	result = TRUE;
 	if (is_empty(input))
+		result = FALSE;
+	else if (has_unclosed_quotes(input))
 		result = FALSE;
 	else if (is_invalid_syntax(input))
 	{
